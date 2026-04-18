17fbd72cbe58d24f5565f86efdf0e4b494e8fa85
RivkaSchonfeld <rivkashon5@gmail.com>
2026-04-19 00:52:06 +0300

    Fix typo in response message

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('You probabaly did not want to see this!');
+    res.send('You probably did not want to see this!');
 });
 
 app.use("/render", r)
